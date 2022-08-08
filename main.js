4061b84caf9f30a51f336906f8a992c1ce3352b2
Scott <ScottAdelman97@gmail.com>
2022-08-08 10:34:39 -0600

    exercises

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 /* Exercise 1 */ 
-const firstName = "Jake" 
-let lastName = "Hammond"
-var age = 23; 
+const firstName = "Scott"; 
+let lastName = "Adelman";
+var age = 25; 
 
 /* Exercise 2 */ 
-firstName = "Scott"
-lastName = "Adelman"
+firstName = "Scott";
+lastName = "Adelman";
 age = 25; 
 
 /* Exercise 3 */ 
-let language = "JavaScript"
-let createdYear = 1995
-let isCaseSensitive = true
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
 /* Exercise 4 */ 
-let price = 19.99
-let isOnSale = false
-let salesPercentage = 15
-let stock = 0
-let inStock = false
-let selectedSize = "M"
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
 
 /* Exercise 5 */ 
-let title = "Name of the Wind"
-let author = "Patrick Rothfuss"
-let pageCount = 722
-let bookmark = 456
-let hasRead = true
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
