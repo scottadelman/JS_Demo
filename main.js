52705488553a4dac52828efdce66d5e2ffbee74d
Scott <ScottAdelman97@gmail.com>
2022-08-08 10:25:05 -0600

    exercises

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Exercise 1 */ 
+const firstName = "Jake" 
+let lastName = "Hammond"
+var age = 23; 
+
+/* Exercise 2 */ 
+firstName = "Scott"
+lastName = "Adelman"
+age = 25; 
+
+/* Exercise 3 */ 
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+/* Exercise 4 */ 
+let price = 19.99
+let isOnSale = false
+let salesPercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+/* Exercise 5 */ 
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
